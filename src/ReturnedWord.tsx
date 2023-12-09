ed8f8d65d8d14fba81136e605d1fa28b58358bf3
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-12-09 20:36:04 +0100

    hello and audio on one line

--- src/ReturnedWord.tsx
+++ src/ReturnedWord.tsx
@@ -6,61 +6,86 @@ type ReturnedWordProps = {
 };
 
 function ReturnedWord({ definition }: ReturnedWordProps) {
-    return (
-      <Container>
-        <TextContainer>
-          {definition ? (
-            <div>
+  return (
+    <Container>
+      <TextContainer>
+        {definition ? (
+          <div>
+            <WordAndPhoneticContainer>
               <h2>{definition.word}</h2>
-              {/* Render phonetics if audio is present */}
-              {definition.phonetics.map((phonetic, index) =>
-                phonetic.audio ? (
-                  <div key={index}>
-                    {phonetic.text && <p>Phonetic Text: {phonetic.text}</p>}
-                    <p>
-                      Audio:{" "}
-                      <a href={phonetic.audio} target="_blank" rel="noopener noreferrer">
-                        Listen
-                      </a>
-                    </p>
+              {definition.phonetics.map((phonetic, index) => (
+                <PhoneticContainer key={index}>
+                  {phonetic.text && (
+                    <PhoneticText>{phonetic.text}</PhoneticText>
+                  )}
+                  {phonetic.audio && (
+                    <AudioLink
+                      href={phonetic.audio}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Listen
+                    </AudioLink>
+                  )}
+                </PhoneticContainer>
+              ))}
+            </WordAndPhoneticContainer>
+            {definition.meanings.map((meaning, index) => (
+              <div key={index}>
+                <BlueText>{meaning.partOfSpeech}</BlueText>
+                {meaning.definitions.map((def, defIndex) => (
+                  <div key={defIndex}>
+                    <StyledList>
+                      <li>{def.definition}</li>
+                    </StyledList>
+                    {def.synonyms.length > 0 && (
+                      <p>Synonyms: {def.synonyms.join(", ")}</p>
+                    )}
+                    {def.antonyms.length > 0 && (
+                      <p>Antonyms: {def.antonyms.join(", ")}</p>
+                    )}
+                    {def.example && <p>Example: {def.example}</p>}
                   </div>
-                ) : null
-              )}
-              {/* Render meanings, definitions, synonyms, antonyms, and examples if present */}
-              {definition.meanings.map((meaning, index) => (
-                <div key={index}>
-                  <BlueText>{meaning.partOfSpeech}</BlueText>
-                  {meaning.definitions.map((def, defIndex) => (
-                    <div key={defIndex}>
-                        <StyledList>
-                        <li>{def.definition}</li>
-                        </StyledList>
-                      
-                      {def.synonyms.length > 0 && <p>Synonyms: {def.synonyms.join(', ')}</p>}
-                      {def.antonyms.length > 0 && <p>Antonyms: {def.antonyms.join(', ')}</p>}
-                    </div>
-                  ))}
-                  {meaning.synonyms.length > 0 && <p>Synonyms: {meaning.synonyms.join(', ')}</p>}
-                  {meaning.antonyms.length > 0 && <p>Antonyms: {meaning.antonyms.join(', ')}</p>}
-                </div>
-              ))}
-              {/* Render license if present */}
-              {definition.license && (
-                <Text>License: <a href={definition.license.url} target="_blank" rel="noopener noreferrer">{definition.license.name}</a></Text>
-              )}
-              {/* Render source URLs if present */}
-              {definition.sourceUrls.map((url, index) => (
-                <StyledLink key={index} href={url} target="_blank" rel="noopener noreferrer">{url}</StyledLink>
-              ))}
-            </div>
-          ) : (
-            <p>No word found</p>
-          )}
-        </TextContainer>
-      </Container>
-    );
-  }
-  
+                ))}
+                {meaning.synonyms.length > 0 && (
+                  <p>Synonyms: {meaning.synonyms.join(", ")}</p>
+                )}
+                {meaning.antonyms.length > 0 && (
+                  <p>Antonyms: {meaning.antonyms.join(", ")}</p>
+                )}
+              </div>
+            ))}
+            {definition.license && (
+              <Text>
+                License:{" "}
+                <StyledLink
+                  href={definition.license.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {definition.license.name}
+                </StyledLink>
+              </Text>
+            )}
+            {definition.sourceUrls.map((url, index) => (
+              <StyledLink
+                key={index}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {url}
+              </StyledLink>
+            ))}
+          </div>
+        ) : (
+          <p>No word found</p>
+        )}
+      </TextContainer>
+    </Container>
+  );
+}
+
 export default ReturnedWord;
 
 const Container = styled.div`
@@ -69,30 +94,27 @@ const Container = styled.div`
   max-width: 40rem;
   display: flex;
   flex-direction: column;
-  /* justify-content: center;
-  align-items: center; */
   margin: 0 auto;
   margin-bottom: 1rem;
-  /* text-align: left; */
 
-  @media(max-width: 768px) {
+  @media (max-width: 768px) {
     max-width: 20rem;
   }
 
-  @media(min-width: 769px) {
+  @media (min-width: 769px) {
     max-width: 40rem;
   }
 `;
 
 const TextContainer = styled.div`
-margin: 1rem;
-`
+  margin: 1rem;
+`;
 
 const BlueText = styled.p`
-color: var(--color-tech-blue);
-margin-top: 2rem;
-font-weight: 600;
-`
+  color: var(--color-tech-blue);
+  margin-top: 2rem;
+  font-weight: 600;
+`;
 
 const StyledList = styled.ul`
   list-style-type: none;
@@ -100,10 +122,27 @@ const StyledList = styled.ul`
 `;
 
 const Text = styled.p`
-margin-top: 2rem;
-font-size: 12px;
-`
+  margin-top: 2rem;
+  font-size: 12px;
+`;
 
 const StyledLink = styled.a`
-font-size: 12px;
-`
+  font-size: 12px;
+`;
+
+const WordAndPhoneticContainer = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+
+const PhoneticContainer = styled.span`
+  margin-left: 10px;
+`;
+
+const PhoneticText = styled.span`
+  font-size: 14px;
+`;
+
+const AudioLink = styled.a`
+  margin-left: 10px;
+`;
