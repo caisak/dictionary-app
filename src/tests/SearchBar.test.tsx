b9a49f3beb12d1972e0c5989a94531502993e6f8
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-12-10 22:46:12 +0100

    tests for search bar

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import SearchBar from '../SearchBar';
+import { useState } from 'react';
+import { vi } from 'vitest';
 
 describe('SearchBar Component', () => {
   it('should display an error message when form is submitted with empty input', () => {
@@ -10,4 +12,33 @@ describe('SearchBar Component', () => {
 
     expect(screen.getByText('Please enter a word to search!')).toBeInTheDocument();
   });
+});
+
+it('should update the input value when typing', () => {
+  const TestComponent = () => {
+    const [word, setWord] = useState('');
+    return <SearchBar word={word} setWord={setWord} setDefinition={() => {}} />;
+  };
+
+  render(<TestComponent />);
+  const input = screen.getByPlaceholderText('Type a word here') as HTMLInputElement;
+  fireEvent.change(input, { target: { value: 'hello' } });
+  expect(input.value).toBe('hello');
+});
+
+it('should not display an error message when a valid word is submitted', async () => {
+  const mockSetWord = vi.fn();
+  render(<SearchBar word="hello" setWord={mockSetWord} setDefinition={() => {}} />);
+  const submitButton = screen.getByText('Search');
+  fireEvent.click(submitButton);
+  expect(screen.queryByText('Please enter a word to search!')).not.toBeInTheDocument();
+});
+
+it('should display an error message when form is submitted with empty input', () => {
+  render(<SearchBar word="" setWord={() => {}} setDefinition={() => {}} />);
+  
+  const submitButton = screen.getByText('Search');
+  fireEvent.click(submitButton);
+
+  expect(screen.getByText('Please enter a word to search!')).toBeInTheDocument();
 });
